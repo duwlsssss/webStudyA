b531b93f9f76913d5e106e6a5f449172bee6e04b
duwlsssss <rladuwls0814@gmail.com>
2024-11-08 17:26:21 +0900

    fix: 로그인 토큰 리프레시 수정

--- movie-project/src/api/apiClient.js
+++ movie-project/src/api/apiClient.js
@@ -7,36 +7,6 @@ const api = axios.create({
   baseURL: API_BASE_URL, 
 });
 
-// 토큰 갱신 함수
-const refreshAccessToken = async () => {
-  const refreshToken = localStorage.getItem('refreshToken');
-  if (!refreshToken) throw new Error("refresh token이 없음");
-
-  try {
-    console.log("갱신 요청 시작 - refreshToken:", refreshToken);
-    const response = await api.post(API_ENDPOINTS.REFRESH_TOKEN, null, {
-      headers: {
-        Authorization: `Bearer ${refreshToken}`,
-      },
-    });
-
-    // 응답 데이터 유효성 검사
-    console.log("갱신 응답 데이터:", response.data);
-    console.log(response)
-    const { accessToken, refreshToken } = response.data;
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
-
-    return accessToken;
-  } catch (error) {
-    console.log("갱신 요청 시작 - refreshToken:", refreshToken);
-    console.log("갱신 응답 데이터:", response.data);
-    console.log(response)
-    console.error('토큰 갱신 중 오류 발생:', error);
-    throw error;
-  }
-};
-
 // 요청을 인터셉트해서 요청마다 accessToken을 추가
 // api.interceptors.request.use((config) => {
 //   const accessToken = localStorage.getItem('accessToken');
@@ -53,12 +23,21 @@ const refreshAccessToken = async () => {
 //   }
 //   return config;
 // });
+
 api.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem('AccessToken');
-    if (accessToken) {
+    const accessToken = localStorage.getItem('accessToken');
+     // accessToken이 필요 없는 엔드포인트 리스트
+    const publicEndpoints = [
+      API_ENDPOINTS.LOGIN,       
+      API_ENDPOINTS.SIGNUP,      
+    ];
+    
+    // 요청 URL이 publicEndpoints에 포함되지 않는 경우에만 Authorization 헤더 추가
+    if (accessToken && !publicEndpoints.includes(config.url)) {
       config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
+    
     return config;
   },
   (error) => {
@@ -76,18 +55,20 @@ api.interceptors.response.use(
 
       try {
         const refreshToken = localStorage.getItem('refreshToken');
+        console.log("갱신 요청 시작 - refreshToken:", refreshToken);
 
         const response = await api.post(API_ENDPOINTS.REFRESH_TOKEN, {
           refreshToken
         });
 
         const { accessToken } = response.data;
-
         localStorage.setItem('accessToken', accessToken);
+
         originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
-        return axiosInstance(originalRequest);
-      } catch (refreshError) {
-        console.error('리프레시 토큰 오류:', refreshError);
+        return axios(originalRequest);
+
+      } catch (error) {
+        console.error('토큰 오류:', error);
       }
     }
 
